8176240cd04ac787e927c3270102c2c9d4d37396
Alenti95 <gusevvo95@gmail.com>
2023-03-05 20:25:29 +0300

    check login url

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOG_IN = (By.CSS_SELECTOR, '[id="login_form"]')
+    REGISTER = (By.CSS_SELECTOR, '[id="register_form"]')
