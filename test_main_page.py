cb1d44e3575c5d58085be6b30d1b6c381bf4588d
Alenti95 <gusevvo95@gmail.com>
2023-03-08 23:42:17 +0300

    lesson 4.3.8 перенос методов из MainPage в BasePage

--- test_main_page.py
+++ test_main_page.py
@@ -57,3 +57,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.go_to_add_product()
     page.solve_quiz_and_get_code()
     BasketPage(browser, browser.current_url).should_not_be_success_message_disappear()
+
+#Открываем страницу, видим переход на логин
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+#Открываем страницу, переходим на страницу логина
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
